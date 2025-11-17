5434c2a800d2ef14ee38589854121439a8b6c92a
Leonor Joaquim <leonorjoaquim98@gmail.com>
2025-11-17 16:10:29 +0000

    feat(loginOption): working on login Page

---
+++ Api/Api/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiAndreLeonorProjetoFinal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto request)
+        {
+            var user = await _context.Users
+                .SingleOrDefaultAsync(u => u.Email == request.Email);
+
+            if (user == null)
+                return Unauthorized("Utilizador não encontrado");
+
+            // Verificar password (hash)
+            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+                return Unauthorized("Password incorrecta");
+
+            var token = GerarJwtToken(user);
+            return Ok(new { token });
+        }
+
+    }
+}
